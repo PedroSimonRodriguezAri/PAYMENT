f32b3c779180cfbe401c9cf0f4418c424d35d5fc
Pedro Simon Rodriguez Ari <https://github.com/PedroSimonRodriguezAri/practicas.git>
2024-09-30 19:00:09 -0400

    proyecto

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\DestinationController;
+use App\Http\Controllers\PackageController;
+use App\Http\Controllers\PaymentController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('clients', ClientController::class);
+Route::resource('destinations', DestinationController::class);
+Route::resource('packages', PackageController::class);
+Route::resource('payment', PaymentController::class);
+
